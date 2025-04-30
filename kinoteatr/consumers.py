7f61d1a0a46952cb4e8a8b9c219277531f829933
Artur <proxaker1111@gmail.com>
2025-04-30 13:38:04 +0300

    wb added

--- kinoteatr/consumers.py
+++ kinoteatr/consumers.py
@@ -1,9 +1,24 @@
-from channels.generic.websocket import WebsocketConsumer
 import json
 import random, time
+
+
+from channels.generic.websocket import WebsocketConsumer
+from kino.models import Locals, Midlle_locals, Film
+from django.shortcuts import get_object_or_404
+
 class KinoWb(WebsocketConsumer):
+
     def connect(self):
-        self.accept() 
-
-
+        self.accept()
         
+        
+        
+
+    def receive(self, text_data):
+        curent_film = get_object_or_404(Film, name=text_data)
+        locals = Midlle_locals.objects.filter(film_key = curent_film)
+        count = locals[0].count_of_locals
+      
+        
+
+        self.send(json.dumps({"message": count}))
